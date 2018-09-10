5ba0bd950c12097afe275432a6e659f5761fff10
aihtshamali <14024020154@umt.edu.pk>
2018-09-10 08:09:12 +0500

    MUltipleJobDOne

--- app/Province.php
+++ app/Province.php
@@ -7,11 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Province extends Model
 {
     public function ApplicantDetail()
-    {   
+    {
         return $this->hasMany('App\ApplicantDetail');
     }
     public function Districts()
-    {   
+    {
         return $this->hasMany('App\District');
     }
 }
